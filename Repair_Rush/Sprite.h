e07c71c5250a90b783e3161caa79172d4e743d4c
Stephen Bruner III <sabruneriii@coloradomesa.edu>
2025-03-02 18:05:23 -0700

    Cursor implementation

--- Repair_Rush/Sprite.h
+++ Repair_Rush/Sprite.h
@@ -5,59 +5,83 @@
 
 using namespace std;
 
+/*
+Removing acceleration and random velocity generation. Making default velocity 0
+Renaming isActive to be isTouching to see if the cursor is over a tool
+Made new function isActive to return value of new member bool active
+Made functions for setting active, vy, and vx
+*/
 class Sprite:public Tile {
-   float ax,ay,vx,vy,x,y; 
+   float vx,vy,x,y; 
+   bool active;
    public:
-   bool didCollide(Sprite &other){
-    if (this==&other) return false; // can't collide with ourselves
-    return ((other.x<x && x < other.x+other.rect.w) && 
-            (other.y<y && y < other.y+other.rect.h));   
+  //  bool didCollide(Sprite &other){
+  //   if (this==&other) return false; // can't collide with ourselves
+  //   return ((other.x<x && x < other.x+other.rect.w) && 
+  //           (other.y<y && y < other.y+other.rect.h));   
+  // }
+  bool isTouching(Sprite &tool) {//funciton to check if the user has selected the tool
+    if (this==&tool) return false; // cursor cant select cursor
+    return ((tool.x<x && x < tool.x+tool.rect.w) && 
+            (tool.y<y && y < tool.y+tool.rect.h));   
   }
-  bool isActive(Sprite &cursor) {//funciton to check if the user has selected the tool
-    if (this==&cursor) return false; // cursor cant select curspr
-    return ((cursor.x<x && x < cursor.x+cursor.rect.w) && 
-            (cursor.y<y && y < cursor.y+cursor.rect.h));   
+
+  bool isActive()
+  {
+    return active;
   }
-  Sprite(MediaManager *mm,
-    float x0=0,float y0=0,
-    float vx0=0,float vy0=0,
-    float ax0=0,float ay0=0,string fname="face.bmp")
-      :Tile(mm,fname)
-      {
+
+  void setActive(bool b)
+  {
+    active = b;
+  }
+
+  void setVy(float vy0)
+  {
+    vy += vy0;
+  }
+
+  void setVx(float vx0)
+  {
+    vx += vx0;
+  }
+
+  Sprite(MediaManager *mm, string fname="face.bmp", float x0=0,float y0=0,
+    float vx0=0,float vy0=0):Tile(mm,fname)
+    {
+        active = true;
        // cout << "Constructor Sprite" << x0 << ' ' << y0 << endl;
-       ax=ax0;
-       ay=ay0;
-       if (vx0==-1) vx=rand() % (100 - -100) + -100;  // rand()%(max-min)+min;
-       else vx=vx0;
-       if (vy0==-1) vy=rand() % (100 - -100) + -100;
-       else vy=vy0;
-       if (x0==-1) x=rand() % 800;
-       else x=x0;
-       if (y0==-1) y=rand()%600;
-       else y=y0; 
+      //  if (vx0==-1) vx=rand() % (100 - -100) + -100;  // rand()%(max-min)+min;
+      //  else
+        vx=vx0;
+      //  if (vy0==-1) vy=rand() % (100 - -100) + -100;
+      //  else
+        vy=vy0;
+      //  if (x0==-1) x=rand() % 800;
+      //  else
+        x=x0;
+      //  if (y0==-1) y=rand()%600;
+      //  else
+        y=y0; 
     }   
-   void bounceX(){ vx*=-1; }
-   void bounceY() { vy*=-1; }
-   void bounce(){ // Bounce in both x and y
-      bounceX();
-      bounceY(); 
-    } 
+  //  void bounceX(){ vx*=-1; }
+  //  void bounceY() { vy*=-1; }
+  //  void bounce(){ // Bounce in both x and y
+  //     bounceX();
+  //     bounceY(); 
+    //} 
    virtual void loop(float dt){ // dt in seconds
-      vx+=ax*dt;
-      vy+=ay*dt;
       x+=vx*dt;
       y+=vy*dt;
       rect.x=x;
       rect.y=y;
-      if (rect.x>800-rect.w) vx*=-1;
-      if (rect.x<1)vx*=-1;
-      if (rect.y>600-rect.h) {
-        ay*=-1;
-        vy*=-1;
-      }
-      if (rect.y<1) {
-        ay*=-1;
-        vy*=-1;
-      }
+      // if (rect.x>800-rect.w) vx*=-1;
+      // if (rect.x<1)vx*=-1;
+      // if (rect.y>600-rect.h) {
+      //   vy*=-1;
+      // }
+      // if (rect.y<1) {
+      //   vy*=-1;
+      // }
    }
 };
--- Repair_Rush/simple.cpp
+++ Repair_Rush/simple.cpp
@@ -10,25 +10,32 @@
  
 using namespace std;
 
+/*
+Reformatted the Sprite constructor, implemented the cursor, cursor control,
+and the ability to select tools with the cursor. Removed the inclusion of
+Animation.h in the makefile. Need to still make the inventory system
+*/
+
 class MyGame:public Game {
     float dt;
-    Sprite *bf,*bf2,*bf3,*bf4;
+    Sprite *bf,*bf2,*bf3,*bf4, *cursor;
     string tool;
     int n;
     Tile *back;
   public:
     MyGame(int level=1):Game(){
         ifstream in;
+        cursor = new Sprite(getMM(),"hand2.bmp",0,0);
         in.open("loadtools.txt"); 
         in>>tool;
         srand(level);
-        bf=new Sprite(getMM(),600,225,0,0,0,0,tool);
+        bf=new Sprite(getMM(),tool,600,225);
         in>>tool;
-        bf2=new Sprite(getMM(),300,270,0,0,0,0,tool);
+        bf2=new Sprite(getMM(),tool,300,270);
         in>>tool;
-        bf3=new Sprite(getMM(),450,225,0,0,0,0,tool);
+        bf3=new Sprite(getMM(),tool,450,225);
         in>>tool;
-        bf4=new Sprite(getMM(),150,270,0,0,0,0,tool);
+        bf4=new Sprite(getMM(),tool,150,270);
         cout<<tool<<endl;
         in.close();
         back=new Tile(getMM());
@@ -39,18 +46,71 @@ class MyGame:public Game {
         bf2->loop(dt);
         bf3->loop(dt);
         bf4->loop(dt);
+        cursor->loop(dt);
         back->render(getRen());
-        bf->render(getRen());
-        bf2->render(getRen());
-        bf3->render(getRen());
-        bf4->render(getRen());
+        cursor->render(getRen());
+        //if the tool is active, render on screen
+        //if not, do not render
+        if(bf->isActive())
+          bf->render(getRen());
+        if(bf2->isActive())
+          bf2->render(getRen());
+        if(bf3->isActive())
+          bf3->render(getRen());
+        if(bf4->isActive())
+          bf4->render(getRen());
         SDL_RenderPresent(getRen()); 
         SDL_Delay(1000.0*dt);
         SDL_Event event;
         if (SDL_PollEvent(&event)){
           if (event.type==SDL_KEYDOWN){
             if (event.key.keysym.sym==SDLK_ESCAPE) running=false;
-            if (event.key.keysym.sym==SDLK_SPACE) bf->bounce();
+            if (event.key.keysym.sym==SDLK_w) cursor->setVy(-5);
+            if (event.key.keysym.sym==SDLK_s) cursor->setVy(5);
+            if (event.key.keysym.sym==SDLK_a) cursor->setVx(-5);
+            if (event.key.keysym.sym==SDLK_d) cursor->setVx(5);
+            if (event.key.keysym.sym==SDLK_SPACE)
+            {
+              if(bf->isActive())
+              {
+                if(cursor->isTouching(*bf))
+                {
+                  if(bf->isActive())
+                  bf->setActive(false);
+                  cout << "Soldering Iron Selected\n";
+                }
+              }
+
+              if(bf2->isActive())
+              {
+                if(cursor->isTouching(*bf2))
+                {
+                  bf2->setActive(false);
+                  cout << "Air Canister Selected\n";
+                }
+              }
+
+              if(bf3->isActive())
+              {
+                if(cursor->isTouching(*bf3))
+                {
+                  bf3->setActive(false);
+                  cout << "Thermal Paste Selected\n";
+                }
+              }
+
+              if(bf4->isActive())
+              {
+                if(cursor->isTouching(*bf4))
+                {
+                  bf4->setActive(false);
+                  cout << "Tweezers Selected\n";
+                }
+              }
+              else
+                cout << "None selected\n";
+            }
+            // if (event.key.keysym.sym==SDLK_SPACE) bf->bounce();
           }
           if (event.type==SDL_WINDOWEVENT){
             if (event.window.event==SDL_WINDOWEVENT_CLOSE) running=false;
@@ -63,6 +123,7 @@ class MyGame:public Game {
       delete bf3;
       delete bf4;
       delete back;
+      delete cursor;
     }
 };
 
