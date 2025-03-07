20b05da02473d2e7521cffda15fa96600376bfff
carodriguezama <carlosrodriguezamaro5@gmail.com>
2025-03-06 22:51:56 -0700

    Made it read from text file

--- Repair_Rush/simple.cpp
+++ Repair_Rush/simple.cpp
@@ -18,51 +18,31 @@ Animation.h in the makefile. Need to still make the inventory system
 
 class MyGame:public Game {
     float dt;
-    Sprite *bf,*bf2,*bf3,*bf4,*bf5, *cursor;//create a vector for the tools later on
+    int check, xpos, ypos;
+    Sprite *cursor;
+    vector<Sprite *> tools;
     string tool;
     int n;
     Tile *back;
   public:
     MyGame(int level=1):Game(){
-        ifstream in;
         cursor = new Sprite(getMM(),"hand2.bmp",0,0);
-        in.open("loadtools.txt"); //do this in one function like the animation
-        in>>tool;
-        srand(level);
-        bf=new Sprite(getMM(),tool,600,225);
-        in>>tool;
-        bf2=new Sprite(getMM(),tool,300,270);
-        in>>tool;
-        bf3=new Sprite(getMM(),tool,450,225);
-        in>>tool;
-        bf4=new Sprite(getMM(),tool,150,270);
-        in>>tool;
-        bf5=new Sprite(getMM(),tool,600,25);
-        in.close();
+        ifstream in("loadtools.txt");
+        while(!in.eof()) {
+          in>>tool>>xpos>>ypos;
+          tools.push_back(new Sprite(getMM(),tool,xpos,ypos));
+        }
         back=new Tile(getMM());
         dt=.01; 
     }
     void loop() {
-        bf->loop(dt);
-        bf2->loop(dt);
-        bf3->loop(dt);
-        bf4->loop(dt);
-        bf5->loop(dt);
+        for(auto face:tools) face->loop(dt);
         cursor->loop(dt);
         back->render(getRen());
         cursor->render(getRen());
-        //if the tool is active, render on screen
-        //if not, do not render
-        if(bf->isActive())
-          bf->render(getRen());
-        if(bf2->isActive())
-          bf2->render(getRen());
-        if(bf3->isActive())
-          bf3->render(getRen());
-        if(bf4->isActive())
-          bf4->render(getRen());
-        if(bf5->isActive())
-          bf5->render(getRen());
+        for(auto face:tools)if(face->isActive())
+        face->render(getRen());
+ 
         SDL_RenderPresent(getRen()); 
         SDL_Delay(1000.0*dt);
         SDL_Event event;
@@ -73,71 +53,33 @@ class MyGame:public Game {
             if (event.key.keysym.sym==SDLK_s) cursor->sety(10);
             if (event.key.keysym.sym==SDLK_a) cursor->setx(-10);
             if (event.key.keysym.sym==SDLK_d) cursor->setx(10);
-            if (event.key.keysym.sym==SDLK_SPACE)
-            {
-              if(bf->isActive())
+            if (event.key.keysym.sym==SDLK_SPACE) {
+              for(auto face:tools) {
+                if(face->isActive())
               {
-                if(cursor->isTouching(*bf))
+                if(cursor->isTouching(*face))
                 {
-                  if(bf->isActive())
-                  bf->setActive(false);
-                  cout << "Soldering Iron Selected\n";
+                  if(face->isActive())
+                  face->setActive(false);
                 }
               }
-
-              if(bf2->isActive())
-              {
-                if(cursor->isTouching(*bf2))
-                {
-                  bf2->setActive(false);
-                  cout << "Air Canister Selected\n";
-                }
               }
-
-              if(bf3->isActive())
-              {
-                if(cursor->isTouching(*bf3))
-                {
-                  bf3->setActive(false);
-                  cout << "Thermal Paste Selected\n";
-                }
-              }
-
-              if(bf4->isActive())
-              {
-                if(cursor->isTouching(*bf4))
-                {
-                  bf4->setActive(false);
-                  cout << "Tweezers Selected\n";
-                }
-              }
-              if(bf5->isActive())
-              {
-                if(cursor->isTouching(*bf5))
-                {
-                  bf5->setActive(false);
-                  cout << "ScrewDriver Selected\n";
-                }
-              }
-              else
-                cout << "None selected\n";
             }
-            // if (event.key.keysym.sym==SDLK_SPACE) bf->bounce();
           }
           if (event.type==SDL_WINDOWEVENT){
             if (event.window.event==SDL_WINDOWEVENT_CLOSE) running=false;
           }
         }
 
-        if(!bf->isActive() && !bf2->isActive() && !bf3->isActive() && !bf4->isActive() && !bf5->isActive())
-          running = false;
+        check = 0;
+
+        for(auto face:tools) {
+          if(!face->isActive()) check++;
+        }
+        if(check==5) running = false;
     }
     ~MyGame(){
-      delete bf;
-      delete bf2;
-      delete bf3;
-      delete bf4;
-      delete bf5;
+      for(auto face:tools) delete face;
       delete back;
       delete cursor;
     }
